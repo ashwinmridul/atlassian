38b9b02f85a798111068edc88a3b96f2eadb287a
Ashwin Mridul <ashwin.mridul1@myntra.com>
2024-06-02 22:30:42 +0530

    Updated tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const timeElement = screen.getByText(/current time/i);
-  expect(timeElement).toBeInTheDocument();
+describe('App', (): void => {
+  it('renders learn react link', (): void => {
+    render(<App />);
+    const timeElement = screen.getByText(/current time/i);
+    expect(timeElement).toBeInTheDocument();
+  });
 });
