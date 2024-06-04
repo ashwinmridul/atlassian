58ed4d5912630794aa0aedee2e084dc5f1609693
Ashwin Mridul <ashwin.mridul1@myntra.com>
2024-06-04 13:34:31 +0530

    Added test cases for seconds

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -4,31 +4,39 @@ describe('getCurrentTime', () => {
     beforeEach(() => {
         jest
             .useFakeTimers()
-            .setSystemTime(new Date('2022-07-25T14:30:00.000Z'));
+            .setSystemTime(new Date('2022-07-25T14:30:23.000Z'));
     });
 
     afterEach(() => {
         jest.useRealTimers();
     });
 
-    it('returns the current time in HH:mm format', () => {
-        expect(getCurrentTime()).toBe('14:30:00');
+    it('returns the current time in HH:mm:ss format', () => {
+        expect(getCurrentTime()).toBe('14:30:23');
     });
 
     it('pads hours with leading zero if necessary', () => {
         // Mock the Date constructor to return a date with hours < 10
         jest
             .useFakeTimers()
-            .setSystemTime(new Date('2022-07-25T08:30:00.000Z'));
-        expect(getCurrentTime()).toBe('08:30:00');
+            .setSystemTime(new Date('2022-07-25T08:30:23.000Z'));
+        expect(getCurrentTime()).toBe('08:30:23');
     });
 
     it('pads minutes with leading zero if necessary', () => {
         // Mock the Date constructor to return a date with minutes < 10
         jest
             .useFakeTimers()
-            .setSystemTime(new Date('2022-07-25T14:05:00.000Z'));
-        expect(getCurrentTime()).toBe('14:05:00');
+            .setSystemTime(new Date('2022-07-25T14:05:23.000Z'));
+        expect(getCurrentTime()).toBe('14:05:23');
+    });
+
+    it('pads seconds with leading zero if necessary', () => {
+        // Mock the Date constructor to return a date with minutes < 10
+        jest
+            .useFakeTimers()
+            .setSystemTime(new Date('2022-07-25T14:05:09.000Z'));
+        expect(getCurrentTime()).toBe('14:05:09');
     });
 
     it('returns a string', () => {
