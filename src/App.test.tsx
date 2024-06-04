52a389fafff66291a93be5f27ab454aa2501a677
Ashwin Mridul <ashwin.mridul1@myntra.com>
2024-06-04 09:04:17 +0530

    Indent

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', (): void => {
-  it('renders date component', (): void => {
-    render(<App />);
-    const timeElement = screen.getByTestId('date');
-    expect(timeElement).toBeInTheDocument();
-  });
+    it('renders date component', (): void => {
+        render(<App />);
+        const timeElement = screen.getByTestId('date');
+        expect(timeElement).toBeInTheDocument();
+    });
 });
--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -3,11 +3,11 @@ import { getFormattedDateString } from './'
 describe('getFormattedDateString', () => {
     it('returns a formatted date string in default locale', () => {
         expect(getFormattedDateString(new Date('2022-07-25 14:30:00'))).toBe('7/25/2022 2:30:00 PM');
-      });
+    });
     
-      it('does not throw an error with an invalid date', () => {
+    it('does not throw an error with an invalid date', () => {
         const invalidDate: Date = new Date(' invalid date ');
         expect(() => getFormattedDateString(invalidDate)).not.toThrowError();
         expect(getFormattedDateString(invalidDate)).toBe('Invalid Date Invalid Date');
-      });
-  });
+    });
+});
